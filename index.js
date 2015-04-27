cb6d9332677568b1b9f3d4618e602bdea0258b59
Seth Kranzler <sethkranzler@gmail.com>
2015-04-26 20:50:19 -0400

    added jquery for dropdowns

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
+var path = require('path'); //added this but not using it yet - try implementing instead of __dirname
 
-app.listen(3000);
+var serialport = require('serialport'), // include the library
+    SerialPort = serialport.SerialPort; // make a local instance of it
+
+app.listen(3000, function(){
+    console.log('started app on 3000');
+});
 
 //serving the index file 
 function handler(req, res) {
-    fs.readFile(__dirname + '/index.html',
+    console.log('got req for ',req);
+    fs.readFile(__dirname + '/index.html',//this __dirname is a problem, try implementing 'path' 
+        //perhaps process.env.pwd or execpath = path.dirname(process.execpath);
         function(err, data) {
             if (err) {
                 res.writeHead(500);
@@ -18,8 +26,6 @@ function handler(req, res) {
         });
 }
 
-var serialport = require('serialport'), // include the library
-    SerialPort = serialport.SerialPort; // make a local instance of it
 
 serialport.list(function(err, ports) { // listing out serial ports and populates list
     $(document).ready(function() {
@@ -27,61 +33,58 @@ serialport.list(function(err, ports) { // listing out serial ports and populates
         ports.forEach(function(port) {
             $("#serialDropdown").append("<option value=\"" + port.comName + "\">" + port.comName + "</option>");
         });
+        //populating the baud rate dropdown
+        var baudArray = [300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200];
+        for (i = 0, max = baudArray.length; i < max; i++) {
+            $("#baudDropdown").append("<option value=\"" + baudArray[i] + "\">" + baudArray[i] + "</option>");
+        }
     })
 });
 
-$(function() {
-    //populating the baud rate dropdown
-    var baudArray = [300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200];
-    for (i in baudArray) $("#baudDropdown").append("<option value=\"" + baudArray[i] + "\">" + baudArray[i] + "</option>");
-});
-//tried moving serial and baud rate variable definitions into functions but now having scope issues
-var setSerialPort = function() {
-        var portName = serialDropdown.options[serialDropdown.selectedIndex].value; //sets portname to selected dropdown value
-}
+var startFunction = function() {
+    var portName = $("#serialDropdown").val();
 
-var setBaudRate = function() {
     var myPort = new SerialPort(portName, {
-        baudRate: baudDropdown.options[baudDropdown.selectedIndex].value,
-        // look for return and newline at the end of each data packet:
-        parser: serialport.parsers.readline("\r\n")
+        baudRate: $("#baudDropdown").val(), // look for return and newline at the end of each data packet:
+        parser: serialport.parsers.readline("\r\n"),
     }, false);
-}
 
-myPort.open(function(error) {
-    if (error) {
-        console.log('failed to open: ' + error);
-    } else {
-        console.log('open');
-    }
 
-    io.on('connection', function(socket) {
-        socket.on('ready', function(data) {
-            console.log('node socket received:', data);
-            myPort.write("into the serial port\n", function(err, results) {
-                if (err) {
-                    console.log('error writing to serial:', err);
-                }
-                console.log('serial results', results);
+    myPort.open(function(error) {
+        if (error) {
+            console.log('failed to open: ' + error);
+        } else {
+            console.log('open');
+        }
+
+        io.on('connection', function(socket) {
+            socket.on('ready', function(data) {
+                console.log('node socket received:', data);
+                myPort.write("into the serial port\n", function(err, results) {
+                    if (err) {
+                        console.log('error writing to serial:', err);
+                    }
+                    console.log('serial results', results);
+                });
             });
-        });
 
-        socket.on('dataFromBrowser', function(data) {
-            console.log('got data from browser:', data);
-            myPort.write(data.message, function(err) {
-                if (err) {
-                    console.log('err from serial:', err);
-                }
+            socket.on('dataFromBrowser', function(data) {
+                console.log('got data from browser:', data);
+                myPort.write(data.message, function(err) {
+                    if (err) {
+                        console.log('err from serial:', err);
+                    }
+                });
             });
-        });
 
-        myPort.on("data", function(data) {
-            var dataIn = data.toString();
-            socket.emit('serialPortData', {
-                data: dataIn
+            myPort.on("data", function(data) {
+                var dataIn = data.toString();
+                socket.emit('serialPortData', {
+                    data: dataIn
+                });
+                //console.log(data);
             });
-            //console.log(data);
-        });
 
+        });
     });
-});
+}
